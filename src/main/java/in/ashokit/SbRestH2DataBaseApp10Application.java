d9d3283ed92e6f88796fb0386c10bbcf2900152c
unknown <jaganji716>
2022-05-19 10:23:52 +0530

    first commit

---
+++ src/main/java/in/ashokit/SbRestH2DataBaseApp10Application.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestH2DataBaseApp10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestH2DataBaseApp10Application.class, args);
+	}
+
+}
